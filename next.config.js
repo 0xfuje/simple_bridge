15cc921eacc573219c1bf0663c4ab02b54d394a2
Bence Pintér <80958740+web3wolf@users.noreply.github.com>
2022-01-17 18:55:36 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    PRIVATE_KEY: '0xc577c681946f542ffc7cbbeca924ba6bf268378f521be53d5d9a56fd67b9af2a'
-  },
-}
-
-module.exports = nextConfig
